aafe2c46a3b08b441d9de0cbb66fb988d51893dd
= <garvitgalgat@gmail.com>
2020-11-23 22:52:15 +0530

    functions created

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_mysqldb import MySQL
 import yaml
 import requests
-
+import json
 
 app = Flask(__name__)
 
 dev = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
+
+pdf = dev['pdf_maker']
+merger = dev['pdf_merger']
 
 app.config['MYSQL_HOST'] = dev['mysql_host']
 app.config['MYSQL_USER'] = dev['mysql_user']
@@ -22,56 +25,85 @@ baseUrlFile = "https://api.telegram.org/file/bot{}".format(api_token)
 def start():    
     r = requests.get(baseUrl+"/getUpdates")
     data = r.json()
-    print(len(data['result']))
     for i in data['result']:
         id = i['message']['from']['id']
-        createUser(id)
+        offset_value = i['update_id']
+        createUser(id, offset_value)
 
-        if 'text' in i['message'] and i['message']['text'] = "/start":
-            removeAllOldImages(id)
+        if 'text' in i['message'] and i['message']['text'] == "/start":
+            removeAllOldImages(id,offset_value)
         if 'document' in i['message']:
-            fileId = i['message']['document']['thumb']['file_id']
-            p = requests.get(baseUrl+"/getFile?fileid={}".format(fileId))
+            fileId = i['message']['document']['file_id']
+            p = requests.get(baseUrl+"/getFile?file_id={}".format(fileId))
             fileDetails = p.json()
             file_path = fileDetails['result']['file_path']
             link = baseUrlFile+"/{}".format(file_path)
             newImage(link, id)
+        if 'text' in i['message'] and i['message']['text'] == "/pdf":
+            make_pdfs(id)
 
-    return str(data['result'][0]['message']['from']['id'])
+    return "Success"
 
 
-def createUser(userID):
+def createUser(userID, offset_value):
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM users")
     users = cur.fetchall()
-    print(users)
     present = False
     for user in users:
         if(str(user[0]) == str(userID)):
             present = True
             print("Already Present")
+            break
 
     if(not present):
-        cur.execute("INSERT INTO users VALUES('{}');".format(userID))  
-        print("INSERTED")
+        cur.execute("INSERT INTO users VALUES('{}', '{}');".format(userID, offset_value))  
         cur.execute("CREATE TABLE user_{}(image VARCHAR(2000));".format(userID))
         mysql.connection.commit()
         cur.close()
-        print("Inserted")
+        print("user_", userID, " inserted to users and table created")
 
 def newImage(link, userId):
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO user_{} VALUES({});".format(userId, link))
+    cur.execute("INSERT INTO user_{} VALUES('{}');".format(userId, link))
     cur.connection.commit()
     cur.close()
-    print(link, " added to user_", userId)
+    print(link, "added to user_", userId)
 
-def removeAllOldImages(userId):
+def removeAllOldImages(userId, offset_value):
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM user_{}".format(userId))
+    cur.execute("UPDATE users SET offset_value='{}' where userId = '{}';".format(offset_value, userId))
     cur.connection.commit()
     cur.close()
     print("Old Images removed")
+
+def make_pdfs(userId):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM user_{}".format(userId))
+    images = cur.fetchall()
+    pdfs = []
+    for image in images:
+        data = '{"Parameters": [{"Name": "File", "FileValue": {"Url": "' + image[0] +  '"}}, {"Name": "StoreFile", "Value": true}]}'
+        print(data)
+        output = json.loads(data)
+        img_pdf = requests.post(pdf, json=output)
+        final_pdf = img_pdf.json()
+        pdfs.append(final_pdf['Files'][0]['Url'])
+    print(pdfs)
+
+    # MERGE PDFs
+    data = '''{"Parameters": [{"Name": "Files","FileValues": ['''
+    for i in range(len(pdfs)):
+        data += '{"Url": "' + pdfs[i] +'"}'
+        if i != len(pdfs)-1:
+            data += ","
+    data += ']},{"Name": "StoreFile","Value": true}]}'
+    print(data)
+    output = json.loads(data)
+    merged_pdf = requests.post(pdf, json=output)
+    print(merged_pdf.json())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
