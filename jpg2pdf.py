1730eb576caac8007fbbf8e7880b7f19e270daec
= <garvitgalgat@gmail.com>
2020-11-24 22:55:45 +0530

    base file added

---
+++ jpg2pdf.py
@@ -0,0 +1,34 @@
+import requests, json, yaml
+
+
+links = ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"]
+dev = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
+url_pdf = dev['pdf_maker']
+url_merge = dev['pdf_merger']
+pdf_links = []
+for i in links:
+    data = '{"Parameters": [{"Name": "File","FileValue": {"Url":"' + i + '"}},{"Name": "StoreFile","Value": true}]}'
+    print(data)
+    output = json.loads(data)
+
+    fin = requests.post(url_pdf, json=output)
+    response = fin.json()
+    print(response['Files'][0]['Url'])
+    pdf_links.append(response['Files'][0]['Url'])
+
+
+data = '{"Parameters": [{"Name": "Files","FileValues": ['
+
+for i in range(len(pdf_links)):
+    data += '{"Url": " '+ pdf_links[i] +'"}'
+    if(i != len(pdf_links)-1):
+        data += ","
+
+data += ']},{"Name": "StoreFile","Value": true}]}'
+
+print(data)
+output = json.loads(data)
+fin = requests.post(url_merge, json=output)
+response = fin.json()
+print(response)
+ 
